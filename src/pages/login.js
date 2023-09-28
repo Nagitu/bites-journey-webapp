7b15a42ea24aec3c2faa2041c2629ff419f4fd6f
Nagitu <denipraa1@gmail.com>
2023-09-28 12:58:36 +0700

    feat: (FE) Login : "simple login page"

---
+++ src/pages/login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lakukan validasi atau kirim data login ke server di sini
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')" }}>
+      <div className="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+        <div className="text-white">
+          <div className="mb-8 flex flex-col items-center">
+            <img src="https://www.logo.wine/a/logo/Instagram/Instagram-Glyph-Color-Logo.wine.svg" width="150" alt="" srcSet="" />
+            <h1 className="mb-2 text-2xl">Instagram</h1>
+            <span className="text-gray-300">Enter Login Details</span>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4 text-lg">
+              <input
+                className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                type="text"
+                name="email"
+                placeholder="id@email.com"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+
+            <div className="mb-4 text-lg">
+              <input
+                className="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+                type="password"
+                name="password"
+                placeholder="*********"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            <div className="mt-8 flex justify-center text-lg text-black">
+              <button type="submit" className="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
